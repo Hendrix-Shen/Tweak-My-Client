0d641b0a915326dab2dda18e892a169d91f7f492
Hendrix-Shen <HendrixShen@qq.com>
2021-05-07 19:49:33 +0800

    move logger to ModInitializer class

--- src/main/java/top/hendrixshen/TweakMyClient/Reference.java
+++ src/main/java/top/hendrixshen/TweakMyClient/Reference.java
@@ -1,11 +1,7 @@
 package top.hendrixshen.TweakMyClient;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Reference {
     public static final String MOD_ID = "tweakmyclient";
     public static final String MOD_NAME = "TweakMyClient";
     public static final String MOD_VERSION = "1.0.2-build.undefined";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 }
--- src/main/java/top/hendrixshen/TweakMyClient/TweakMyClient.java
+++ src/main/java/top/hendrixshen/TweakMyClient/TweakMyClient.java
@@ -4,19 +4,19 @@ import fi.dy.masa.malilib.config.ConfigManager;
 import fi.dy.masa.malilib.event.InputEventHandler;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.MinecraftClient;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import top.hendrixshen.TweakMyClient.config.Configs;
 import top.hendrixshen.TweakMyClient.event.InputHandler;
 
 public class TweakMyClient implements ModInitializer {
-    public static Logger LOGGER;
+    public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
     public static MinecraftClient minecraftClient = MinecraftClient.getInstance();
 
     @Override
     public void onInitialize() {
         ConfigManager.getInstance().registerConfigHandler(Reference.MOD_ID, new Configs());
-        LOGGER = Reference.LOGGER;
         InputEventHandler.getKeybindManager().registerKeybindProvider(InputHandler.getInstance());
-        LOGGER.info(Reference.MOD_NAME + "-" + Reference.MOD_VERSION + " initialized.");
+        logger.info(Reference.MOD_NAME + "-" + Reference.MOD_VERSION + " initialized.");
     }
 }
