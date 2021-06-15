6ab88a92b247a494e59a05f585e9c5e4b18f5674
Hendrix-Shen <HendrixShen@qq.com>
2021-06-15 23:33:33 +0800

    Fix language file `zh_cn` cannot be loaded

--- src/main/java/top/hendrixshen/TweakMyClient/Reference.java
+++ src/main/java/top/hendrixshen/TweakMyClient/Reference.java
@@ -3,5 +3,5 @@ package top.hendrixshen.TweakMyClient;
 public class Reference {
     public static final String MOD_ID = "tweakmyclient";
     public static final String MOD_NAME = "TweakMyClient";
-    public static final String MOD_VERSION = "1.1.0-build.undefined";
+    public static final String MOD_VERSION = "1.1.1-build.undefined";
 }
