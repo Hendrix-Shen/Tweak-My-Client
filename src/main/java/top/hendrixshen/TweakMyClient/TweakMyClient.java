94754d9c48c447f8bc3fc6de266a925fb05b116d
Hendrix-Shen <HendrixShen@qq.com>
2021-07-18 03:29:23 +0800

    Modify version format.

--- src/main/java/top/hendrixshen/TweakMyClient/TweakMyClient.java
+++ src/main/java/top/hendrixshen/TweakMyClient/TweakMyClient.java
@@ -23,7 +23,7 @@ public class TweakMyClient implements ModInitializer {
 
         IRenderer renderer = new RenderHandler();
         RenderEventHandler.getInstance().registerWorldLastRenderer(renderer);
-        logger.info(String.format("[%s]: Mod initialized - Version: %s ", Reference.MOD_NAME, Reference.MOD_VERSION));
+        logger.info(String.format("[%s]: Mod initialized - Version: %s (%s)", Reference.MOD_NAME, Reference.MOD_VERSION, Reference.MOD_VERSION_TYPE));
         logger.info(String.format("[%s]: AuthMe was %sdetectect.", Reference.MOD_NAME, (TweakMyClientMixinPlugin.isAuthMeLoaded ? "" : "not ")));
         logger.info(String.format("[%s]: Litematica was %sdetectect.", Reference.MOD_NAME, (TweakMyClientMixinPlugin.isLitematicaLoaded ? "" : "not ")));
         logger.info(String.format("[%s]: ReAuth was %sdetectect.", Reference.MOD_NAME, (TweakMyClientMixinPlugin.isReAuthLoaded ? "" : "not ")));
