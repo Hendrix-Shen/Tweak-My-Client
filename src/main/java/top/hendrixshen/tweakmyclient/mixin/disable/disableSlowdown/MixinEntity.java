4fa67d514a889cac9138799f2ee8eca92cae4e12
Hendrix-Shen <HendrixShen@hendrixshen.top>
2022-06-17 14:28:00 +0800

    Hope it works (#12)

--- src/main/java/top/hendrixshen/tweakmyclient/mixin/disable/disableSlowdown/MixinEntity.java
+++ src/main/java/top/hendrixshen/tweakmyclient/mixin/disable/disableSlowdown/MixinEntity.java
@@ -1,37 +1,25 @@
 package top.hendrixshen.tweakmyclient.mixin.disable.disableSlowdown;
 
-//#if MC < 11500
-//$$ import net.minecraft.client.Minecraft;
-//#else
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.Entity;
-//#endif
-//#if MC >= 11700
 import net.minecraft.world.phys.Vec3;
 import org.objectweb.asm.Opcodes;
-//#endif
 
 import org.spongepowered.asm.mixin.Mixin;
-//#if MC >= 11500
-//#if MC >= 11700
 import org.spongepowered.asm.mixin.Shadow;
-//#endif
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-//#if MC >= 11700
-import org.spongepowered.asm.mixin.injection.Redirect;
-//#endif
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import top.hendrixshen.tweakmyclient.config.Configs;
-//#endif
 //#if MC >= 11500
+import org.spongepowered.asm.mixin.injection.Inject;
+//#endif
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+//#if MC >= 11500
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+//#endif
+import top.hendrixshen.tweakmyclient.config.Configs;
 
 @Mixin(Entity.class)
-//#else
-//$$ @Mixin(Minecraft.class)
-//#endif
 public class MixinEntity {
-    //#if MC >= 11700
     @Shadow
     protected Vec3 stuckSpeedMultiplier;
 
@@ -47,7 +35,6 @@ public class MixinEntity {
     private Vec3 onGetStuckSpeedMultiplier(Entity instance) {
         return Configs.disableSlowdown && instance instanceof LocalPlayer ? Vec3.ZERO : this.stuckSpeedMultiplier;
     }
-    //#endif
 
     //#if MC >= 11500
     @Inject(
