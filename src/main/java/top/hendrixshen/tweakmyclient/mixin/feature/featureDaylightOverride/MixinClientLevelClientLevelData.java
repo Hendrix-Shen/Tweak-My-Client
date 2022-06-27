bef5a15e31c7950b3dec5c22b447217068e4b4c9
Hendrix-Shen <HendrixShen@hendrixshen.top>
2022-06-27 14:46:33 +0800

    Format code

--- src/main/java/top/hendrixshen/tweakmyclient/mixin/feature/featureDaylightOverride/MixinClientLevelClientLevelData.java
+++ src/main/java/top/hendrixshen/tweakmyclient/mixin/feature/featureDaylightOverride/MixinClientLevelClientLevelData.java
@@ -1,15 +1,15 @@
 package top.hendrixshen.tweakmyclient.mixin.feature.featureDaylightOverride;
 
-//#if MC < 11600
-//$$ import net.minecraft.client.Minecraft;
-//#else
+//#if MC >= 11600
 import net.minecraft.client.multiplayer.ClientLevel;
-//#endif
-import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import top.hendrixshen.tweakmyclient.config.Configs;
+//#else
+//$$ import net.minecraft.client.Minecraft;
+//#endif
+import org.spongepowered.asm.mixin.Mixin;
 
 //#if MC >=11600
 @Mixin(ClientLevel.ClientLevelData.class)
