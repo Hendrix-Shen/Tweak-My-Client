fcf399583620f95578a3b46b529e18251c78ac46
Hendrix-Shen <HendrixShen@hendrixshen.top>
2025-03-14 11:14:51 +0800

    Add MC 1.21.4 support
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- src/main/java/top/hendrixshen/tweakmyclient/mixin/disable/disableRenderOverlayFire/MixinScreenEffectRenderer.java
+++ src/main/java/top/hendrixshen/tweakmyclient/mixin/disable/disableRenderOverlayFire/MixinScreenEffectRenderer.java
@@ -1,7 +1,6 @@
 package top.hendrixshen.tweakmyclient.mixin.disable.disableRenderOverlayFire;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ScreenEffectRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,10 +8,25 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.hendrixshen.tweakmyclient.game.Configs;
 
+//#if MC > 12103
+//$$ import net.minecraft.client.renderer.MultiBufferSource;
+//#else
+import net.minecraft.client.Minecraft;
+//#endif
+
 @Mixin(ScreenEffectRenderer.class)
 public abstract class MixinScreenEffectRenderer {
     @Inject(method = "renderFire", at = @At("HEAD"), cancellable = true)
-    private static void onRenderFire(Minecraft minecraft, PoseStack poseStack, CallbackInfo ci) {
+    private static void onRenderFire(
+            //#if MC < 12104
+            Minecraft minecraft,
+            //#endif
+            PoseStack poseStack,
+            //#if MC > 12103
+            //$$ MultiBufferSource multiBufferSource,
+            //#endif
+            CallbackInfo ci
+    ) {
         if (Configs.disableFireOverlayRender.getBooleanValue()) {
             ci.cancel();
         }
