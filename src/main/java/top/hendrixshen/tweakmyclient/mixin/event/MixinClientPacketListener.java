7cd7a0bf294829b3527afc8e794eccbc055db3ce
Hendrix-Shen <HendrixShen@hendrixshen.top>
2025-04-04 01:12:44 +0800

    Fix crash when joining single player
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- src/main/java/top/hendrixshen/tweakmyclient/mixin/event/MixinClientPacketListener.java
+++ src/main/java/top/hendrixshen/tweakmyclient/mixin/event/MixinClientPacketListener.java
@@ -1,5 +1,6 @@
 package top.hendrixshen.tweakmyclient.mixin.event;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.protocol.game.ClientboundLoginPacket;
 import org.spongepowered.asm.mixin.Mixin;
@@ -7,13 +8,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.hendrixshen.magiclib.impl.event.EventManager;
-import top.hendrixshen.magiclib.util.MiscUtil;
 import top.hendrixshen.tweakmyclient.impl.event.LocalPlayerEvent.LocalPlayerGameJoinEvent;
 
 @Mixin(ClientPacketListener.class)
 public class MixinClientPacketListener {
     @Inject(method = "handleLogin", at = @At(value = "RETURN"))
     private void onGameJoin(ClientboundLoginPacket clientboundLoginPacket, CallbackInfo ci) {
-        EventManager.dispatch(new LocalPlayerGameJoinEvent(MiscUtil.cast(this)));
+        EventManager.dispatch(new LocalPlayerGameJoinEvent(Minecraft.getInstance().player));
     }
 }
