0675e6874855a31cfd640d140b64eca4c3f96843
Hendrix-Shen <HendrixShen@hendrixshen.top>
2026-09-06 18:16:21 +0800

    Fix compatible with Litematica
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- src/main/java/top/hendrixshen/tweakmyclient/mixin/patch/disableLitematicaSchematicVersionCheck/MixinLitematicaSchematic.java
+++ src/main/java/top/hendrixshen/tweakmyclient/mixin/patch/disableLitematicaSchematicVersionCheck/MixinLitematicaSchematic.java
@@ -1,66 +1,58 @@
 package top.hendrixshen.tweakmyclient.mixin.patch.disableLitematicaSchematicVersionCheck;
 
 import fi.dy.masa.litematica.schematic.LitematicaSchematic;
-import fi.dy.masa.litematica.schematic.SchematicMetadata;
 import top.hendrixshen.magiclib.api.dependency.annotation.Dependencies;
 import top.hendrixshen.magiclib.api.dependency.annotation.Dependency;
 import top.hendrixshen.tweakmyclient.game.Configs;
 
-// CHECKSTYLE.OFF: ImportOrder
-//#if MC >= 12105
-//$$ import fi.dy.masa.litematica.config.Configs.Generic;
-//$$ import fi.dy.masa.litematica.util.FileType;
-//#endif
-// CHECKSTYLE.ON: ImportOrder
-
-import net.minecraft.nbt.CompoundTag;
-
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import top.hendrixshen.magiclib.libs.com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 @Dependencies(require = @Dependency(value = "litematica"))
 @Mixin(value = LitematicaSchematic.class, remap = false)
 public abstract class MixinLitematicaSchematic {
-    @Shadow
-    @Final
-    private SchematicMetadata metadata;
-
-    @Shadow
-    protected abstract void readSubRegionsFromNBT(CompoundTag tag, int version, int minecraftDataVersion);
-
-    @Inject(
-            method = "readFromNBT",
-            at = @At(
-                    value = "INVOKE",
-                    //#if MC >= 12105
-                    //$$ target = "Lnet/minecraft/nbt/CompoundTag;getIntOr(Ljava/lang/String;I)I",
-                    //#else
-                    target = "Lnet/minecraft/nbt/CompoundTag;getInt(Ljava/lang/String;)I",
-                    //#endif
-                    remap = true,
-                    ordinal = 0
-            ),
-            cancellable = true
-    )
-    private void ignoreVersionCheck(CompoundTag nbt, CallbackInfoReturnable<Boolean> cir) {
-        if (Configs.disableLitematicaSchematicVersionCheck.getBooleanValue()) {
-            //#if MC >= 12105
-            //$$ int version = nbt.getIntOr("Version", -1);
-            //$$ int minecraftDataVersion = nbt.getIntOr("MinecraftDataVersion", Generic.DATAFIXER_DEFAULT_SCHEMA.getIntegerValue());
-            //$$ this.metadata.readFromNBT(nbt.getCompoundOrEmpty("Metadata"));
-            //$$ this.metadata.setSchematicVersion(version);
-            //$$ this.metadata.setMinecraftDataVersion(minecraftDataVersion);
-            //$$ this.metadata.setFileType(FileType.LITEMATICA_SCHEMATIC);
-            //$$ this.readSubRegionsFromNBT(nbt.getCompoundOrEmpty("Regions"), version, minecraftDataVersion);
+    // The upstream method rejects schematics when their version is outside the supported range
+    // (1..5 on MC <= 1.16, 1..6 on MC 1.17-1.20.4, 1..7 on newer MC). Instead of duplicating the
+    // whole loading logic (which is fragile across Litematica API changes), widen the two bounds
+    // of the `version >= min && version <= max` check so that it always evaluates to true.
+    @ModifyExpressionValue(
+            //#if MC >= 1.21.11
+            //$$ method = "readFromData",
             //#else
-            this.metadata.readFromNBT(nbt.getCompound("Metadata"));
-            this.readSubRegionsFromNBT(nbt.getCompound("Regions"), nbt.getInt("Version"), nbt.getInt("MinecraftDataVersion"));
+            method = "readFromNBT",
             //#endif
-            cir.setReturnValue(true);
-        }
+            at = @At(
+                    value = "CONSTANT",
+                    args = "intValue=1",
+                    ordinal = 0
+            )
+    )
+    private int disableSchematicVersionLowerBound(int original) {
+        return Configs.disableLitematicaSchematicVersionCheck.getBooleanValue() ? Integer.MIN_VALUE : original;
+    }
+
+    @ModifyExpressionValue(
+            //#if MC >= 1.21.11
+            //$$ method = "readFromData",
+            //#else
+            method = "readFromNBT",
+            //#endif
+            at = @At(
+                    value = "CONSTANT",
+                    //#if MC >= 1.20.6
+                    //$$ args = "intValue=7",
+                    //#else
+                    //#if MC >= 1.17.1
+                    //$$ args = "intValue=6",
+                    //#else
+                    args = "intValue=5",
+                    //#endif
+                    //#endif
+                    ordinal = 0
+            )
+    )
+    private int disableSchematicVersionUpperBound(int original) {
+        return Configs.disableLitematicaSchematicVersionCheck.getBooleanValue() ? Integer.MAX_VALUE : original;
     }
 }
